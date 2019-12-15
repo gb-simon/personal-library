a5a54c97ab3a1927f653705e339cfee0c16276f0
Glitch (quick-drum) <none>
2019-12-15 08:03:20 +0000

    🎋✂️ Updated with Glitch

--- server.js
+++ server.js
@@ -16,31 +16,25 @@ app.use(helmet.hidePoweredBy({setTo: 'PHP 4.2.0'}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
-
+app.use(cors({origin: '*'})); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
-//For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
 apiRoutes(app);  
     
-//404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
 });
 
-//Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port " + process.env.PORT);
   if(process.env.NODE_ENV==='test') {
@@ -57,4 +51,4 @@ app.listen(process.env.PORT || 3000, function () {
   }
 });
 
-module.exports = app; //for unit/functional testing
+module.exports = app; 
